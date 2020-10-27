27d55052c24fc4d9ac44cc50a975a21ae09a7d15
Gaven Rudiak <gavenrudiak@outlook.com>
2020-10-27 09:57:24 -0400

    breaktime

--- WPF Crazy PC/MainWindow.xaml.cs
+++ WPF Crazy PC/MainWindow.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -13,6 +15,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace WPF_Crazy_PC
 {
     /// <summary>
@@ -20,10 +23,60 @@ namespace WPF_Crazy_PC
     /// </summary>
     public partial class MainWindow : Window
     {
+        int score;
+        int x;
+        int y;
+        
+        public Random r = new Random();
+
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (Convert.ToString(xButton.Content) == "Start")
+            {
+                StartGame();
+            }
+            else
+            {
+                Score();
+            }
+        }
+
+        public void StartGame()
+        {
+            //start timer
+            Thread timerThreadStart = new Thread(new ThreadStart(Timer));
+            timerThreadStart.Start();
+            //change name 
+            xButton.Content = "Click Me!";
+
+        }
+
+        public void Timer()
+        {
+            int time = 0;
+            while (time >= 60)
+            {
+                xTimer.Text = Convert.ToString(60 - time);
+                Thread.Sleep(1000);
+                time++;
+            }
+        }
+
+        public void Score()
+        {
+            //adds score and prints
+            score++;
+            xScore.Text = Convert.ToString($"Score: {score}");
+            //move button
+            y = r.Next(1,400);
+            x = r.Next(1,800);
+            xButton.Margin = new Thickness(x,y,0,0);
+        }
     }
 }
